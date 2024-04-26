cc22d59e7f2e7e73d1ae4f521549a7841b7abdb0
saranVishva <99174518+saranVishva@users.noreply.github.com>
2024-04-26 12:09:37 +0530

    Update splunk_installer.sh

--- AWS_SCRIPT/splunk_installer.sh
+++ AWS_SCRIPT/splunk_installer.sh
@@ -1,8 +1,5 @@
-#!/bin/bash
-echo "SPLUNK INSTALLER"
-
 # Username and password
-username="spower"
+username="splunker"
 password="password"
 
 # Create the user if it doesn't exist
@@ -16,12 +13,15 @@ fi
 
 # Install Splunk
 echo "Downloading Splunk installer..."
-sudo wget -O /tmp/splunk-9.1.2-b6b9c8185839-Linux-x86_64.tgz "https://download.splunk.com/products/splunk/releases/9.1.2/linu>
+sudo wget -O /opt/splunk-9.1.2-b6b9c8185839-Linux-x86_64.tgz "https://download.splunk.com/products/splunk/releases/9.1.2/linux/splunk-9.1.2-b6b9c8185839-Linux-x86_64.tgz"
 
 # Switch to splunk user and install
 cd /opt/
-sudo tar xvf /tmp/splunk-9.1.2-b6b9c8185839-Linux-x86_64.tgz
+sudo tar xvf /opt/splunk-9.1.2-b6b9c8185839-Linux-x86_64.tgz
 sudo chown -R "$username:$username" /opt/splunk
-command_to_execute="/opt/splunk/bin/splunk start --accept-license --answer-yes --no-prompt --seed-passwd "$password""
+command_to_execute="/opt/splunk/bin/splunk start --accept-license --answer-yes --no-prompt --seed-passwd \"$password\""
 sudo -u "$username" bash -c "$command_to_execute"
+
+#Systemd and enable boot splunk
+sudo -u "$username" /opt/splunk/bin/splunk enable boot-start -user "$username" -systemd-managed 1
 echo "Splunk installed under user $username."
